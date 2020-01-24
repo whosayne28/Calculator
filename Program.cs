e1df67250ab8931c6d5fb4e2cd31bbdce759e890
Abir Hosssain <59974981+whosayne28@users.noreply.github.com>
2020-01-24 13:45:56 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SimpleCalculator calculator = new SimpleCalculator();
+
+			double a = 10;
+			double b = 5;
+
+			Console.WriteLine("{0} + {1} = {2}", a, b, calculator.Add(a,b));
+			Console.WriteLine("{0} * {1} = {2}", a, b, calculator.Multiply(a,b));
+			Console.WriteLine("{0} - {1} = {2}", a, b, calculator.Substract(a,b));
+
+			double devisionResult = calculator.Devide(a, b);
+			if (devisionResult != 0 || a == devisionResult)
+			{
+				Console.WriteLine("{0} / {1} = {2}", a, b, devisionResult);
+			}
+			else
+			{
+				// Division with 0 happened!
+			}
+		}
+	}
+}
---
+++ SimpleCalculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Assignment
+{
+	public class SimpleCalculator
+	{
+		private readonly double kmToMiles = 0.621371;
+		private readonly double kgToPounds = 2.20462;
+
+		#region Basic Mathematical Operations
+
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public double Substract(double a, double b)
+		{
+			return a - b;
+		}
+
+		public double Multiply(double a, double b)
+		{
+			return a * b;
+		}
+
+		public double Devide(double a, double b)
+		{
+			if (b == 0)
+			{
+				Console.WriteLine("Division with 0 is not allowed!");
+				return 0;
+			}
+
+			return a / b;
+		}
+
+		#endregion
+
+		#region Conversions
+
+		public double ConvertMilesToKilometers(double miles)
+		{
+			return miles / kmToMiles;
+		}
+
+		public double ConvertKilometersToMiles(double km)
+		{
+			return km * kmToMiles;
+		}
+
+		public double ConvertKilogramsToPounds(double kg)
+		{
+			return kg * kgToPounds;
+		}
+
+		public double ConvertPoundsToKilograms(double pounds)
+		{
+			return pounds / kgToPounds;
+		}
+
+		#endregion
+	}
+}
